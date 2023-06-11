0b974a47b90b968094fcc0b43cc3ed71da2da0f4
wang <johns10166@gmail.com>
2023-06-11 20:03:56 +0900

    [add] day2

---
+++ day2/day2.go
@@ -0,0 +1,22 @@
+package main
+
+//bool⇒布爾型
+//int 整型
+//int8 8位整型
+//byte 字節型
+
+import (
+	sd "fmt"
+)
+
+type (
+	byte uint8
+	rune int8
+	テキスト string
+)
+
+func main() {
+	a := 65
+	b := string(a)
+	sd.Println(b)
+}
